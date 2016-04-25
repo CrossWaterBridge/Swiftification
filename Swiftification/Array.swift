24028f1c3d405c4d38dbafadbbe6c99b18246397
Hilton Campbell <hiltonc@ldsmobileapps.com>
2016-04-25 07:52:32 -0700

    Use guard instead of if.

--- Swiftification/Array.swift
+++ Swiftification/Array.swift
@@ -51,9 +51,8 @@ public extension Array {
     
     /// Randomly rearranges the elements of self using the Fisher-Yates shuffle
     mutating func shuffleInPlace() {
-        if count <= 1 {
-            return
-        }
+        guard count > 1 else { return }
+        
         for index in (1..<count).reverse() {
             let newIndex = Int.random(0...index)
             if index != newIndex {
